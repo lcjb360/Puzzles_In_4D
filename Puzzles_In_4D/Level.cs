688da129dd6ee4de293cdd9a1e9908571aa82549
Liam Bye <liamcjbye@gmail.com>
2023-01-28 18:02:33 +0000

    Started forming how levels are drawn

---
+++ Puzzles_In_4D/Level.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles_In_4D
+{
+    class Level
+    {
+        List<Object> Objects;
+
+        public void Draw(SpriteBatch spriteBatch, int Player_W_Position)
+        {
+            //Draw order stuff
+        }
+    }
+}
